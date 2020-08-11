7304fa6a26e668587e8541df5063dfb3a97dfb4b
iqbalchoironi <iqbal.choironi@gmail.com>
2020-08-11 20:35:11 +0700

    add confog

--- server.js
+++ server.js
@@ -1,11 +1,27 @@
 const express = require('express');
-const app = require('express');
-
 const app = express();
+
+const { graphqlHTTP } = require('express-graphql');
+const schema = require('./lib/graphql');
+
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost:27017/footbal');
+const db = mongoose.connection;
+db.on('error', () => {
+    console.log('error connect to DB')
+}).once('open', () => {
+    console.log('connected to DB');
+})
 
 app.get('/', (req, res) => {
     res.send('OK');
 });
+
+app.use('/graphql', graphqlHTTP(() => ({
+        schema,
+        graphiql: true,
+        pretty: true
+})));
 
 app.listen(1945, () => {
     console.log('server listen to PORT 1945')
